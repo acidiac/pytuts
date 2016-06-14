ee5f16a1f198ef1f23d8e41b33ae558495b5066f
amit chanchal <amit.chanchal@hotmail.com>
2016-06-14 18:00:31 +0530

    oop files

--- cylinder.py
+++ cylinder.py
@@ -1,10 +1,26 @@
 class Cylinder(object):
+	pi = 3.14
 
 	def __init__(self, height=1, radius=1):
-		pass
+		self.height = height
+		self.radius = radius
 
 	def volume(self):
-		pass
+		h  = self.height
+		r  = self.radius
+		pi = self.pi
+		return pi*(r**2)*h
 
 	def surface_area(self):
-		pass
+		h  = self.height
+		r  = self.radius
+		pi = self.pi
+		top = pi * r**2
+		return (top*2)+ (2*pi*r*h)
+
+
+nc = Cylinder(4,6)
+
+print(nc.volume())
+print(nc.surface_area())
+
--- line.py
+++ line.py
@@ -1,10 +1,30 @@
 class Line(object):
 	def __init__(self, coor1, coor2):
 		#coor are tuples (3,2)
-		pass
+		self.coor1 = coor1
+		self.coor2 = coor2
+
+	def __str__(self):
+		x1,y1 = self.coor1
+		x2,y2 = self.coor2
+		return ("A line between (%d,%d) and (%d,%d)" %(x1,y1,x2,y2))
+
 
 	def distance(self):
-		pass
+		#tuple unpacking
+		x1,y1 = self.coor1
+		x2,y2 = self.coor2
+		print(x1, y1, x2, y2)
+		return ((x2-x1)**2/(y2-y1)**2)**0.5
 
 	def slope(self):
-		pass
+		x1,y1 = self.coor1
+		x2,y2 = self.coor2
+		return (y2-y1)/(x2-x1)
+
+
+# testing the cases
+
+new_line = Line((4,4),(1,0))
+
+print(new_line)
