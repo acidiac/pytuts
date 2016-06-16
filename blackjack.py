dff561de32921ac7565f5fd90ec9fe75c50229be
amit chanchal <amit.chanchal@hotmail.com>
2016-06-16 08:24:24 +0530

    python blackjack

---
+++ blackjack.py
@@ -0,0 +1,24 @@
+# Blackjack text based game
+# amit chanchal
+# version 1
+
+class Player(Object):
+
+	player_moves = []
+
+	player_money = []
+
+	def __init__(self, money):
+		self.money = money
+
+	def add_money(self, amount):
+		self.money += amount
+
+	def reduce_money(self, amount):
+		self.money -= amount
+
+	
+
+class Deck(Object):
+	pass
+	
