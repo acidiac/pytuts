d4712b261dd40e918571177366754036ea929692
amit chanchal <amit.chanchal@hotmail.com>
2016-06-18 23:07:43 +0530

    python ml

---
+++ ml_Stanford/w1_unspl.py
@@ -0,0 +1,24 @@
+'''
+While in Supervised Learning problem we are given a datasets which has labelled and there is an explicit
+reference to the solution
+
+Unsupervised Learning:
+in unsupervised learning we are not given labels with the datasets and we are not told what to do with it. 
+Here we are just given data and we try to find a structure. This structure could take many forms
+Clustering:
+	when the given dataset can be grouped as clusters. This is used in many cases for example
+	-Google News which looks at loads of news from around and groups them into cohesive stories,
+	-Clustering algorithms are also used in genomics for clustering people based on gene expressions
+	-organizing large computer clusters in data center
+	-Social Network analysis to identify cohesive groups
+	-Market segmentation and customer segment discovery
+	-Astronomical data analysis
+Cocktail Party problem:
+	
+
+
+
+
+
+
+'''
