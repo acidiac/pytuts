59618e81a3c573ae292faec69eee5b7eb456b354
amit chanchal <amit.chanchal@hotmail.com>
2016-06-19 23:07:54 +0530

    change to map-reduce

--- map.py
+++ map.py
@@ -1,12 +1,11 @@
 '''
 Map is a built-in function which literally does what the name says, it maps a function to an iterable.
 Syntax:
-	map(function, iterable) 
+	map(function, iterable(s)) 
 	the above syntax will return an object with result of outputs from the iterable(list, set..). This can
 	be converted into a list by simply using the list() function.
 
 '''
-
 
 
 def sft_to_sqm(val):
@@ -20,6 +19,8 @@ area_sft = [23,34,45,67,73,11,89,113]
 area_sqm = list(map(sft_to_sqm, area_sft))
 
 print(area_sqm)
+#[2.1367, 3.1586, 4.180499999999999, 6.2242999999999995, 6.7817, 1.0219, 8.2681, 10.4977]
+
 
 
 # map using lambda expression
@@ -27,4 +28,22 @@ print(area_sqm)
 new_area = list(map(lambda area: area*0.0929, area_sft))
 
 print(new_area)
+# [2.1367, 3.1586, 4.180499999999999, 6.2242999999999995, 6.7817, 1.0219, 8.2681, 10.4977]
+
+# we can use map with more than one iterable
+# this is quite an stupid example but I think this makes it a bit more clearer
+withdrawals = [100,15,23,35]
+deposits 	= [200,45,12,450]
+months = ["jan","feb","mar","apr"]
+
+def acc_state(withdraw, deposit, month):
+	balance = deposit - withdraw
+	return ("month [%s], Monthly Balance: %d " %(month,balance))
+
+statement = list(map(acc_state, withdrawals,deposits,months))
+print(statement) # this is not really a good practice but for now lets see what it does..
+# ['month [jan], Monthly Balance: 100 ', 'month [feb], Monthly Balance: 30 ', 'month [mar], Monthly Balance: -11 ', 'month [apr], Monthly Balance: 415 ']
+
+
+
 
---
+++ reduce.py
@@ -0,0 +1,39 @@
+import functools
+
+'''
+Reduce is a builtin function which takes a sequence and produces one output
+It takes two values and applies function to it and then store it as first value and applies function to the new value
+Syntax:
+	reduce(function, sequence)
+
+	let's say we have a list "numbers" [a,b,c,d] and we want to apply function "add" to get sum of the list
+	
+	def add (a,b):
+		return a + b
+	
+	output_sum = reduce(add, numbers)
+
+	so what is happening here?
+
+	imaging this 
+	----------------------------------
+	a
+	b 	sum1 = [a+b]
+	c 	sum2 = [sum1 + c]
+	d 	sum3 = [sum2 + d]
+
+	output sum3
+	-----------------------------
+
+'''
+
+def add(a,b):
+	return a + b
+
+stuff = [1,2,3,4,5]
+
+output_sum = functools.reduce(add, stuff)
+
+print(output_sum)
+
+
