70a5c697d47c1fcf2aa23890519d3d863a7998b9
amit chanchal <amit.chanchal@hotmail.com>
2016-06-19 22:40:29 +0530

    change to map

--- map.py
+++ map.py
@@ -1,3 +1,14 @@
+'''
+Map is a built-in function which literally does what the name says, it maps a function to an iterable.
+Syntax:
+	map(function, iterable) 
+	the above syntax will return an object with result of outputs from the iterable(list, set..). This can
+	be converted into a list by simply using the list() function.
+
+'''
+
+
+
 def sft_to_sqm(val):
 	'''
 		square feet area into square meters
@@ -9,4 +20,11 @@ area_sft = [23,34,45,67,73,11,89,113]
 area_sqm = list(map(sft_to_sqm, area_sft))
 
 print(area_sqm)
+
+
+# map using lambda expression
+
+new_area = list(map(lambda area: area*0.0929, area_sft))
+
+print(new_area)
 
