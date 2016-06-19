a1ce9a5b245d298781305598a73613493014fd9e
amit chanchal <amit.chanchal@hotmail.com>
2016-06-19 18:00:43 +0530

    more algo files

---
+++ ml_Stanford/sup_ulr1.py
@@ -0,0 +1,20 @@
+'''
+	Fitting Univariate Linear Regression model
+	--------------------------------------------------------------
+
+	Cost Functions:
+
+		hypothesis 
+		h0(x) = theta0 + theta1(x)
+			- thetas in the hypothesis are called parameters of the model
+			- how do we chose the parameter values for the equation
+		parameters
+			theta0 and theta1
+
+		Cost Function
+			j(theta0,theta1) = 1/2m * (summation(m, i=1)[(h* theta0(x1i) - y(i))**2]
+
+		Goal/Objective
+			minimize J(theta0,theta1) for (theta0, theta1)
+
+'''
